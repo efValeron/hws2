ae40d608be225ee59233466bfc28e413dc430a87
Valeriy <masterbox06@gmail.com>
2023-08-17 14:23:33 +0400

    hw1 done, style not done

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -6,56 +6,67 @@ import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
 /*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
+* 1 - описать тип MessageType ------
+* 2 - описать тип MessagePropsType в файле Message.tsx ------
+* 3 - в файле Message.tsx отобразить приходящие данные ------
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx ------
 * 5 - сделать стили в соответствии с дизайном
 * */
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+type SenderType = {
+  avatar: string
+  name: string
+}
+type MessageParamsType = {
+  text: string
+  time: string
+}
 
-// структуру объекта не менять
+export type MessageType = {
+  id: number
+  user: SenderType
+  message: MessageParamsType
+}
+
 export const message0: MessageType = {
-    id: 0,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
-    },
-    message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
-    },
+  id: 0,
+  user: {
+    avatar: avatar,
+    name: 'Some Name',
+  },
+  message: {
+    text: 'some textsome textsome textsome textsome textsome textsome text',
+    time: '22:00',
+  },
 }
 export const friendMessage0: MessageType = {
-    id: 100,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
-    },
-    message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
-    },
+  id: 100,
+  user: {
+    avatar: avatar, // можно менять
+    name: 'Friend Name', // можно менять
+  },
+  message: {
+    text: 'зеркальное сообщение для тренировки css', // можно менять
+    time: '22:00', // можно менять
+  },
 }
 
 const HW1 = () => {
-    return (
-        <div id={'hw1'}>
-            <div className={s2.hwTitle}>Homework #1</div>
-            <div className={s2.hw}>
-                {/*проверка отображения (не менять)*/}
-                <div>
-                    <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
-                </div>
-
-                {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
-            </div>
+  return (
+    <div id={'hw1'}>
+      <div className={s2.hwTitle}>Homework #1</div>
+      <div className={s2.hw}>
+        {/*проверка отображения (не менять)*/}
+        <div>
+          <Message message={message0}/>
+          <FriendMessage message={friendMessage0}/>
         </div>
-    )
+
+        {/*для автоматической проверки дз (не менять)*/}
+        <MessageSender M={Message}/>
+      </div>
+    </div>
+  )
 }
 
 export default HW1

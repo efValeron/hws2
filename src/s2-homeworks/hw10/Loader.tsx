4d56775b5f61a0f4dda5e9f03df3792038aa88e9
Valeriy <masterbox06@gmail.com>
2023-10-25 19:46:41 +0600

    hw10 done

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
-import s from './Loader.module.css'
+import s from "./Loader.module.css"
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"
+       fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+       strokeLinejoin="round" className={s.loader}>
+    <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
+  </svg>
+)
+

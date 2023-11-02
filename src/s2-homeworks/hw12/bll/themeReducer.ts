837d321cfbf90d87af6ebabf766dae28eadce26c
Valeriy <masterbox06@gmail.com>
2023-11-02 11:17:08 +0600

    hw12 styles no so much

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,21 @@
 const initState = {
-    themeId: 1,
+  themeId: 1
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+type RootThemeStateType = typeof initState
 
-        default:
-            return state
-    }
+export const themeReducer = (state: RootThemeStateType = initState, action: ActionType): RootThemeStateType => { // fix any
+  switch (action.type) {
+    case "SET_THEME_ID":
+      return {
+        ...state,
+        themeId: action.id
+      }
+    default:
+      return state
+  }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = ReturnType<typeof changeThemeIdAC>
+
+export const changeThemeIdAC = (id: number) => ({type: "SET_THEME_ID", id} as const)
